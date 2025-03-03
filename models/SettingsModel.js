09011842a8e1aa12219a7c1e24965a15a758ddf1
kimeswartz <kim.e.swartz@gmail.com>
2025-03-03 17:05:46 +0100

    updated to ES modules

--- models/SettingsModel.js
+++ models/SettingsModel.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 const SettingsSchema = new mongoose.Schema({
-  color: {type: String, required: true },
-  logo: {type: String, required: true },
-  campaignImage: {type: String, required: true},
+  color: { type: String, required: true },
+  logo: { type: String, required: true },
+  campaignImage: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Settings', SettingsSchema);
+const Settings = mongoose.model('Settings', SettingsSchema);
+export default Settings;
